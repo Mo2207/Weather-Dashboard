aa01b53d7f4cbf1c2d4d63cc0fe51193d545c001
Gabe Morris <gabemorris1042@gmail.com>
2022-11-25 13:42:07 -0400

    added functionality to display serached city's 5 day forecast, working on displaying current locations 5 day forecast automatically when page loads

--- scripts/script.js
+++ scripts/script.js
@@ -1,62 +1,94 @@
 
-// Fetch from the openweather api
-let sjWeather;
-fetch(`https://api.openweathermap.org/data/2.5/forecast?cnt=1&q=${"saint John"}&units=metric&appid=3a3724d04775e368285b5dbd5c300e67`)
-.then(function (response) {
-  return response.json();
-})
-.then(function (data) {
-  sjWeather = data;
-  $("#locationWeather").text(`Current weather for ${data.city.name}:`);
-  $("#displayTemp").text(`Temperature: ${sjWeather.list[0].main.temp} °C`);
-  $("#displayWind").text(`Wind: ${sjWeather.list[0].wind.speed} km/h`);
-  $("#displayHumidity").text(`Humidity: ${sjWeather.list[0].main.humidity} %`);
-  console.log(sjWeather);
-});
-
-
-
+// checks users current location
 let getLocation = () => {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showLocation);
+    // if users location is enabled, run currentLocationWeather
+    navigator.geolocation.getCurrentPosition(currentLocationWeather);
   } else {
-    console.log("Turn on location to get weather near you.");
+    $("#locationWeather").text(`Turn on location to get weather near you.`);
   }
 }
 
-let showLocation = (position) => {
-  console.log(position.coords.latitude)
-  console.log(position.coords.longitude)
+// displays current weather for current location
+let currentLocationWeather = (position) => {
+
+  // ---------- Current Location current weather ----------
+  // Using lon/lat coordinates, fetch the current weather data for given location
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=3a3724d04775e368285b5dbd5c300e67`)
+  .then(function(response) {
+  return response.json();
+  })
+  .then(function(data) {
+
+  // Display the current weather for the users current location onto page
+  $("#locationWeather").text(`Current weather for ${data.name}:`);
+  $("#displayTemp").text(`Temperature: ${data.main.temp} °C`);
+  $("#displayWind").text(`Wind: ${data.wind.speed} km/h`);
+  $("#displayHumidity").text(`Humidity: ${data.main.humidity} %`);
+  });
+
+  // ---------- Current location 5 day forecast ----------
+  // Fetch the 5 day forecast data for the users current location
+  fetch(`https//api.openweathermap.org/data/2.5/forecast?cnt=5&lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=3a3724d04775e368285b5dbd5c300e67`)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    // Display the 5 day forecast for the users current location onto the page
+    for (let i=0; i < data.list.length; i++) {
+      $(`#cardTemp${i.toString()}`).text(`Temperature: ${data.list[i].main.temp} °C`);
+      $(`#cardWind${i.toString()}`).text(`Wind: ${data.list[i].wind.speed} °C`);
+      $(`#cardHumid${i.toString()}`).text(`Humidity: ${data.list[i].main.humidity} °C`);
+    }
+  })
 }
 
 
 let searchCity = () => {
+  // set cityName to what the user submitted
   let cityName = ($("#search").val()).toString();
+  
+  // reset the textfield after submission
   $("#search").val('');
-  console.log(cityName)
 
   // let openweather api decide if the users searched city is valid
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?cnt=1&q=${cityName}&units=metric&appid=3a3724d04775e368285b5dbd5c300e67`)
-  .then(function (response) {
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?cnt=5&q=${cityName}&units=metric&appid=3a3724d04775e368285b5dbd5c300e67`)
+  .then(function(response) {
   if (response.ok) {
-    console.log("response is ok");
+    // console.log("response is ok");
     return response.json();
   } else {
     alert("That city doesn't exist. Please try again")
   }
   })
   .then(function(data) {
-    $("#locationWeather").text(`Current weather for ${data.city.name}:`);
-    $("#displayTemp").text(`Temperature: ${data.list[0].main.temp} °C`);
-    $("#displayWind").text(`Wind: ${data.list[0].wind.speed} km/h`);
-    $("#displayHumidity").text(`Humidity: ${data.list[0].main.humidity} %`);
-  })
+    console.log(data)
+
+    // Display the current weather for the searched city to the page
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=3a3724d04775e368285b5dbd5c300e67`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      $("#locationWeather").text(`Current weather for ${data.name}:`);
+      $("#displayTemp").text(`Temperature: ${data.main.temp} °C`);
+      $("#displayWind").text(`Wind: ${data.wind.speed} km/h`);
+      $("#displayHumidity").text(`Humidity: ${data.main.humidity} %`);
+    })
+
+    // Display the 5 day forecast for the searched city to the page
+    for (let i=0; i < data.list.length; i++) {
+      $(`#cardTemp${i.toString()}`).text(`Temperature: ${data.list[i].main.temp} °C`);
+      $(`#cardWind${i.toString()}`).text(`Wind: ${data.list[i].wind.speed} °C`);
+      $(`#cardHumid${i.toString()}`).text(`Humidity: ${data.list[i].main.humidity} °C`);
+    }
+    })
 }
 
 $("#searchCity").on("click", searchCity);
 
-
-// getLocation()
+// call function right away to find users location
+getLocation()
 
 
 
