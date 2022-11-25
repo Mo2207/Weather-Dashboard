5a35e10128dafd3e2278ae6557e0622cf4889111
Gabe Morris <gabemorris1042@gmail.com>
2022-11-25 16:47:20 -0400

    cleaned code by creating 2 functions to fetch current/future weather data, less repitition

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 
+let currentDate = dayjs().format("D/MM/YYYY");
+
 // checks users current location
 let getLocation = () => {
   if (navigator.geolocation) {
@@ -10,41 +12,11 @@ let getLocation = () => {
   }
 }
 
-// displays current weather for current location as well as the 5 day forecast
+// displays current weather for current location and the 5 day forecast as soon as page is loaded
 let currentLocationWeather = (position) => {
+  fetchCurrentWeather(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=3a3724d04775e368285b5dbd5c300e67`);
 
-  // ---------- Current Location current weather ----------
-  // Using lon/lat coordinates, fetch the current weather data for users current location
-  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=3a3724d04775e368285b5dbd5c300e67`)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    // Display the current weather for the users current location onto page
-    $("#locationWeather").text(`Current weather for ${data.name}:`);
-    $("#displayTemp").text(`Temperature: ${data.main.temp} °C`);
-    $("#displayWind").text(`Wind: ${data.wind.speed} km/h`);
-    $("#displayHumidity").text(`Humidity: ${data.main.humidity} %`);
-  });
-
-  // ---------- Current location 5 day forecast ----------
-  // Fetch the 5 day forecast data for the users current location
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?cnt=45&lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=3a3724d04775e368285b5dbd5c300e67`)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-
-    // Display the 5 day forecast for the users current location onto the page
-    let cardCount = 1;
-    for (let i=5; i < data.list.length; i+=8) {
-      $(`#cardDate${cardCount.toString()}`).text(`${data.list[i].dt_txt.slice(0,-9)}`);
-      $(`#cardTemp${cardCount.toString()}`).text(`Temperature: ${data.list[i].main.temp} °C`);
-      $(`#cardWind${cardCount.toString()}`).text(`Wind: ${data.list[i].wind.speed} km/h`);
-      $(`#cardHumid${cardCount.toString()}`).text(`Humidity: ${data.list[i].main.humidity} %`);
-      cardCount++;
-    }
-  })
+  fetchFutureForecast(`https://api.openweathermap.org/data/2.5/forecast?cnt=45&lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=3a3724d04775e368285b5dbd5c300e67`);
 }
 
 // searchCity is called when user submits a city name
@@ -65,37 +37,43 @@ let searchCity = () => {
     }
   })
   .then(function(data) {
-    console.log(data)
 
-    // ---------- Once validated, fetch current/5day forecast ----------
-    // Display the current weather for the searched city to the page
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=3a3724d04775e368285b5dbd5c300e67`)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      $("#locationWeather").text(`Current weather for ${data.name}:`);
-      $("#displayTemp").text(`Temperature: ${data.main.temp} °C`);
-      $("#displayWind").text(`Wind: ${data.wind.speed} km/h`);
-      $("#displayHumidity").text(`Humidity: ${data.main.humidity} %`);
-    })
+    // Once validated, fetch the current and the 5 day forecast
+    fetchCurrentWeather(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=3a3724d04775e368285b5dbd5c300e67`);
+    
+    fetchFutureForecast(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&cnt=45&units=metric&appid=3a3724d04775e368285b5dbd5c300e67`);
+  })
+}
 
-    // Display the 5 day forecast for the searched city to the page
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&cnt=45&units=metric&appid=3a3724d04775e368285b5dbd5c300e67`)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
+// Fetches the current weather data
+let fetchCurrentWeather = (url) => {
+  fetch(url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    $("#locationWeather").text(`Current weather for ${data.name} (${currentDate})`);
+    $("#displayTemp").text(`Temperature: ${data.main.temp} °C`);
+    $("#displayWind").text(`Wind: ${data.wind.speed} km/h`);
+    $("#displayHumidity").text(`Humidity: ${data.main.humidity} %`);
+  })
+}
 
-      let cardCount = 1;
-      for (let i=5; i < data.list.length; i+=8) {
-        $(`#cardDate${cardCount.toString()}`).text(`${data.list[i].dt_txt.slice(0,-9)}`);
-        $(`#cardTemp${cardCount.toString()}`).text(`Temperature: ${data.list[i].main.temp} °C`);
-        $(`#cardWind${cardCount.toString()}`).text(`Wind: ${data.list[i].wind.speed} km/h`);
-        $(`#cardHumid${cardCount.toString()}`).text(`Humidity: ${data.list[i].main.humidity} %`);
-        cardCount++;
-      }
-    })
+// Fetches the 5 day forecast
+let fetchFutureForecast = (url) => {
+  fetch(url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    let cardCount = 1;
+    for (let i=5; i < data.list.length; i+=8) {
+      $(`#cardDate${cardCount.toString()}`).text(`${data.list[i].dt_txt.slice(0,-9)}`);
+      $(`#cardTemp${cardCount.toString()}`).text(`Temperature: ${data.list[i].main.temp} °C`);
+      $(`#cardWind${cardCount.toString()}`).text(`Wind: ${data.list[i].wind.speed} km/h`);
+      $(`#cardHumid${cardCount.toString()}`).text(`Humidity: ${data.list[i].main.humidity} %`);
+      cardCount++;
+    }
   })
 }
 
@@ -103,9 +81,9 @@ let searchHistory = () => {
 
   // set cityName to what the user submitted
   let cityName = ($("#search").val()).toString();
-  
+
   let history = $("#history").addClass("badge text-bg-secondary w-100").css("height: 4vh;");
-  $("history").
+  
 }
 
 $("#searchCity").on("click", searchCity);
